7c7b50260e9b26b12c050198040fe886e42573ab
Matvei <57399129+Neenizheb@users.noreply.github.com>
2020-01-15 13:54:38 +0300

    Add files via upload

---
+++ Breast_Neural_Network.py
@@ -0,0 +1,31 @@
+from keras import layers
+import pandas as pd
+from keras.models import Sequential
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+# Data
+data = pd.read_csv('C:/Users/asynk/Desktop/Data/Breast_cancer_data.csv')
+x = data.drop(['diagnosis'], axis=1)
+y = data['diagnosis']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33)
+
+# Model
+model = Sequential()
+# Input layer
+model.add(layers.Dense(150, activation='sigmoid', input_dim=5))
+# Hidden layer
+model.add(layers.Dropout(0.2))
+model.add(layers.Dense(100, activation='sigmoid'))
+# Output layer
+model.add(layers.Dense(1, activation='sigmoid'))
+# Compilation
+model.compile(
+    optimizer='adam',
+    loss='mse',
+    metrics=['accuracy']
+)
+# Training
+model.fit(x_train, y_train, epochs=100)
+# Accuracy
+print(model.evaluate(x_test, y_test))
